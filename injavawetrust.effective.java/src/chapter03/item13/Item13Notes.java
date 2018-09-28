92f00cb21cfc53f2bde4496981b55eab808f32c8
leventerguder <erguder.levent@gmail.com>
2018-09-28 20:39:05 +0300

    Effective Java Item 13

--- injavawetrust.effective.java/src/chapter03/item13/Item13Notes.java
+++ injavawetrust.effective.java/src/chapter03/item13/Item13Notes.java
@@ -17,8 +17,24 @@ public class Item13Notes {
 
 // The general contract for the clone method is weak ;
 // Creates and returns a copy of this object. The precise meaning of "copy" may depend on the class of the object.
-// The general intent is that for any object x ,
-// the expression x.clone() != x will be true 
+// The general intent is that for any object x , the expression x.clone() != x will be true 
+
 // and expression
-// x.clone().getClass() == x.getClass() will be true
-// x.clone().equals(x) will be true
+// x.clone().getClass() == x.getClass() will be true , this is not absolute requirement
+
+// x.clone().equals(x) will be true ,this is not absolute requirement
+
+// By convention, the object returned by this method should be obtained by calling super.clone. 
+// If a class and all of its superclasses (except Object) obey this convention, it will be the case that
+// x.clone().getClass() == x.getClass()
+
+// By convention, the returned object should be independent of the object being cloned. 
+// To achieve this independence, it may be necessary to modify one or more fields of the object returned by super.clone before returning it.
+
+// This mechanism is vaguely similar to constructor chaining, except that it isn’t enforced: 
+// if a class’s clone method returns an instance that is not obtained by calling super.clone but by calling a constructor, 
+// the compiler won’t complain, but if a subclass of that class calls super.clone, the resulting object will have the wrong class, 
+// preventing the subclass from clone method from working properly.
+
+// Suppose you want to implement Cloneable in a class whose superclass provides a well-behaved clone method. 
+// First call super.clone. The object you get back will be a fully functional replica of the original.
